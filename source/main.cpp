cad0454c80fa4275014f4fb9534f9ab5b8dc97ae
Sam Kent <sam.john.kent@gmail.com>
2017-11-15 15:47:55 +0000

    BLE disabled

--- source/main.cpp
+++ source/main.cpp
@@ -24,6 +24,7 @@ DEALINGS IN THE SOFTWARE.
 */
 
 #include "MicroBit.h"
+#include "MicroBitFlash.h"
 
 MicroBit uBit;
 
@@ -34,8 +35,33 @@ int main()
 
     // Setup a simple triangular waveform.
     MicroBitImage img("1 0 0 0 0 0 0 0 0 1\n0 1 0 0 0 0 0 0 1 0\n0 0 1 0 0 0 0 1 0 0\n0 0 0 1 0 0 1 0 0 0\n0 0 0 0 1 1 0 0 0 0\n");
+
+    MicroBitFlash flash;
+    uint32_t buffer1 = 0x12345678;
+    uint32_t buffer2 = 0x87654321;
+
+    int result = 0;
+
+    // Write Buffer 1
+    for(uint32_t x = 0x30000; x < 0x30100; x = x +4){
+        result = flash.flash_write((uint32_t *)x, &buffer1, sizeof(buffer1));
+    }
+
+    uBit.display.print(result);
+    uBit.sleep(1000);
+
+    // Write Buffer 2
+    for(uint32_t y = 0x30080; y < 0x30200; y = y +4){
+        result = flash.flash_write((uint32_t *)y, &buffer2, sizeof(buffer2));
+    }
+   
+    uBit.display.print(result);
+    uBit.sleep(1000);
+
+    uBit.display.print("D");
+
+    while(1);
      
-    while(1)
-        uBit.display.scroll(img, 80, -1);
+
 }
 
